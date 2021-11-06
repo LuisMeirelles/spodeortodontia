af4b19e2b0d50a956d9320a7d8ae5e1cef591aee
Luis Meirelles <luis.gustavo.souto.meirelles@gmail.com>
2021-11-05 23:43:08 -0300

    Basic Preview

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,11 +1,23 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+    :root {
+        --color-primary: #800000;
+        --color-secondary: #003000;
+        --color-secondary-dark: #002A00;
+        --color-tertiary: #D6D6D6;
+    }
+
     * {
         margin:0;
         padding: 0;
         outline: none;
         box-sizing: border-box;
+
+        ::selection {
+            color: var(--color-tertiary);
+            background-color: #002A00;
+        }
     }
 
     html,
@@ -15,8 +27,14 @@ const GlobalStyle = createGlobalStyle`
         min-height: 100vh;
     }
 
+    html {
+        scroll-behavior: smooth;
+    }
+
     body {
         font-family: sans-serif;
+        color: var(--color-primary);
+        background-color: --color-tertiary;
     }
 
     #root {
--- src/styles.js
+++ src/styles.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const Header = styled.header`
     width: 100%;
-    background: #D6D6D6;
+    background-color: var(--color-secondary-dark);
+    color: var(--color-tertiary);
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -12,10 +13,45 @@ export const Header = styled.header`
 
 export const Title = styled.h1`
     text-align: center;
+    color: var(--color-tertiary);
 `;
 
 export const Subtitle = styled.h2`
     text-align: center;
+    color: var(--color-tertiary);
+`;
+
+export const Navbar = styled.nav`
+    background-color: var(--color-secondary);
+    display: flex;
+    justify-content: center;
+    height: 2rem;
+    position: sticky;
+    top: 0;
+`;
+
+export const NavMenu = styled.ul`
+    list-style: none;
+    display: flex;
+    height: 2rem;
+`;
+
+export const NavItem = styled.li`
+    height: 2rem;
+`;
+
+export const NavLink = styled.a`
+    text-decoration: none;
+    color: var(--color-tertiary);
+    padding: 0.5rem;
+    display: block;
+    transition: background-color 0.3s;
+    height: 2rem;
+
+    :hover,
+    :focus {
+        background-color: var(--color-secondary-dark);
+    }
 `;
 
 export const Main = styled.main`
@@ -25,8 +61,10 @@ export const Main = styled.main`
 
 export const Aside = styled.aside`
     width: 25vw;
-    flex-grow: 1;
-    margin-right: 1rem;
+    height: calc(100vh - 7rem);
+    margin-top: 2rem;
+    position: sticky;
+    top: 4rem;
 `;
 
 export const Menu = styled.ul`
@@ -34,34 +72,56 @@ export const Menu = styled.ul`
 `;
 
 export const MenuItem = styled.li`
-    transition: background-color 0.3s;
-
-    :hover,
-    :focus-within {
-        background-color: #c2c2c2;
-    }
 `;
 
 export const LinkItem = styled.a`
     width: 100%;
     display: block;
     text-decoration: none;
-    color: #000000;
+    color: var(--color-primary);
     padding: 0.5rem;
+    transition: background-color 0.3s;
+
+    :hover,
+    :focus {
+        background-color: var(--color-secondary-dark);
+        color: var(--color-tertiary);
+    }
 `;
 
 export const Article = styled.article`
     flex-grow: 1;
+    width: min-content;
+    margin: 0.5rem 1rem 1rem 1rem;
+
+    ul {
+        margin: 1rem 0 1rem 2rem;
+    }
+
+    ol {
+        margin: 1rem 0 1rem 1.5rem;
+    }
+`;
+
+export const Section = styled.section`
+    padding-top: 2rem;
+`;
+
+export const SectionTitle = styled.h2`
+    margin-bottom: 0.5rem;
 `;
 
 export const Paragraph = styled.p`
     margin-bottom: 0.5rem;
+    text-indent: 1.5rem;
 `;
 
 export const Footer = styled.footer`
     display: flex;
     justify-content: center;
     align-items: center;
-    background: #D6D6D6;
+    background: var(--color-secondary-dark);
+    color: var(--color-tertiary);
+    height: 3rem;
     padding: 1rem;
 `;
